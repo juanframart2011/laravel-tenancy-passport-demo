94f7c942ab50391a7f770f2fa11b744908540a82
juanframart2011 <juanframart2011@gmail.com>
2025-02-23 21:47:26 -0600

    ajuste de vue and vuetifiy

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,22 +1,21 @@
+//Imports
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Vuetify from 'vuetify'
+import App from '@/App'
+import routes from '@/routes.js'
 
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+//Load Plugins
+Vue.use(VueRouter)
+Vue.use(Vuetify)
 
-require('./bootstrap');
-
-window.Vue = require('vue');
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+//Router configuration
+const router = new VueRouter({
+    routes 
+  })
 
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+    render: h => h(App),
+    router
 });
